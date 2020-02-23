6c741b185a2e309ab5bfd16740b59ad1102e4354
Anna Kim <annajinee@gmail.com>
2020-02-23 16:09:25 +0900

    진행중..

--- src/home/RequestQnaPage.vue
+++ src/home/RequestQnaPage.vue
@@ -1,6 +1,36 @@
 <template>
-    <h2>상담요청하기</h2>
-
+    <section class="page-section m-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-7 text-center">
+                    <h2 class="mt-0">상담예약</h2>
+                    <hr class="divider my-4">
+                    <h4 class="m-4">무엇이 궁금하세요?</h4>
+                    <p class="text-muted mb-4">이슈파인드에 대한 궁금증을 빠르게 해결해 드리겠습니다.<br>
+                        성함과 전화번호를 남기시고 전화를 받으실 수 있는 시간대를 남겨주세요<br>
+                        전문 상담사가 회원님의 문의사항을 빠르게 처리해드립니다</p>
+                    <input type="text" class="form-control" placeholder="이름을 입력하세요"
+                           style="margin-bottom: 10px"/>
+                    <input type="text" class="form-control" placeholder="전화번호를 숫자만 입력하세요"
+                           style="margin-bottom: 10px"/>
+                    <table>
+                        <td>
+                            <input type="text" class="form-control" placeholder="인증번호 입력"/>
+                        </td>
+                        <td style="padding-left: 10px">
+                            <a class="btn btn-dark btn-group-sm" href="#about">인증번호 받기</a>
+                        </td>
+                    </table>
+                    <div class="m-3">
+                        <input type="checkbox" name="chk_info" value="agreePrivate">개인정보 제공 및 이용동의
+                        <a class="js-scroll-trigger ml-3" href="#about">자세히
+                            보기</a>
+                    </div>
+                    <a class="btn btn-primary btn-lg js-scroll-trigger col-lg-12 mt-1" href="#about">문의하기</a>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
--- src/login/Login.vue
+++ src/login/Login.vue
@@ -22,7 +22,7 @@
                     <router-link to="/signup" class="pl-4">회원가입</router-link>
                 </div>
                 <div class="form-group mt-4">
-                    <button class="btn btn-primary btn-lg js-scroll-trigger col-lg-12 " :disabled="loggingIn">로그인
+                    <button class="btn btn-primary btn-lg js-scroll-trigger col-lg-12" :disabled="loggingIn">로그인
                     </button>
                     <img v-show="loggingIn"
                          src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="/>
